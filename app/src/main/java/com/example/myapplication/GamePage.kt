e408314736f8118339a134840d9d4bafe73ca671
308kyle <47866258+308kyle@users.noreply.github.com>
2022-05-07 11:46:05 -0400

    added some logic to the game

--- app/src/main/java/com/example/myapplication/GamePage.kt
+++ app/src/main/java/com/example/myapplication/GamePage.kt
@@ -1,24 +1,25 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.w3c.dom.Text
+import java.util.*
 
-class GamePage: AppCompatActivity()  {
-    lateinit var one: Button
-    lateinit var two: Button
-    lateinit var three: Button
-    lateinit var four: Button
-    lateinit var five: Button
-    lateinit var six: Button
-    lateinit var seven: Button
-    lateinit var eight: Button
-    lateinit var nine: Button
+class GamePage: AppCompatActivity(), View.OnClickListener {
+
     lateinit var rounds: TextView
-    lateinit var time:TextView
+    lateinit var time: TextView
+    private val sequence: MutableList<Int> = ArrayList()
+    private lateinit var buttons: List<Button>
+    var running: Boolean = false
+    
+    val mHandler: Handler = Handler(Looper.getMainLooper())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +29,25 @@ class GamePage: AppCompatActivity()  {
         //init the round and time fields, should update them as the game goes, but idk
         //better figure out how to track time in kotlin
 
+        buttons = listOf(
+            findViewById(R.id.one),
+            findViewById(R.id.two),
+            findViewById(R.id.three),
+            findViewById(R.id.four),
+            findViewById(R.id.five),
+            findViewById(R.id.six),
+            findViewById(R.id.seven),
+            findViewById(R.id.eight),
+            findViewById(R.id.nine)
+        )
+        for (i in 0..8) {
+            buttons[i].setOnClickListener(this)
+            buttons[i].tag = i
+
+        }
+
         rounds = findViewById(R.id.rounds)
         time = findViewById(R.id.time)
-        //initializing the buttons
-        one = findViewById(R.id.one)
-        two = findViewById(R.id.two)
-        three = findViewById(R.id.three)
-        four = findViewById(R.id.four)
-        five = findViewById(R.id.five)
-        six = findViewById(R.id.six)
-        seven = findViewById(R.id.seven)
-        eight = findViewById(R.id.eight)
-        nine = findViewById(R.id.nine)
 
 //      all the on click listeners for the buttons are below (theres a lose function at the bottom)
 //      in case it isnt obvious these listeners refer to these buttons
@@ -52,40 +60,47 @@ class GamePage: AppCompatActivity()  {
 //              |     |
 //           7  |  8  |  9
 //              |     |
-        one.setOnClickListener{
-
-        }
-        two.setOnClickListener{
-
-        }
-        three.setOnClickListener{
-
-        }
-        four.setOnClickListener{
-
-        }
-        five.setOnClickListener{
-
-        }
-        six.setOnClickListener{
-
-        }
-        seven.setOnClickListener{
-
-        }
-        eight.setOnClickListener{
-
-        }
-        nine.setOnClickListener{
-            lose()
-        }
-
-
-
-
 
     }
+
+    override fun onClick(v: View?) {
+        playSequence()
+        //TODO
+    }
+
+    private fun playSequence() {
+        if (!running) {
+            // locks the user out of clicking
+            running = true
+            mHandler.postDelayed({
+                running = false
+            }, (1000 * sequence.size + 1000).toLong())
+
+            sequence.add(randomButton())
+
+            for (i in 0 until sequence.size) {
+                mHandler.postDelayed({
+                    buttons[sequence[i]].setBackgroundColor(Color.YELLOW)
+                }, (1000 * i).toLong())
+
+                mHandler.postDelayed({
+                    buttons[sequence[i]].setBackgroundColor(Color.BLUE)
+                }, (1000 * i + 1000).toLong())
+            }
+        }
+    }
+
+    private fun randomButton() : Int {
+        val r = Random()
+        return r.nextInt(buttons.size)
+    }
+
     fun lose(){
         //will jump to the loss screen, not really there yet but just call this and ill update this function later
     }
+
+    companion object {
+        private const val TAG = "Corsi-Tapping"
+    }
+
 }
